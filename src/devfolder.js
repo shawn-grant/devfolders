f0d827056addfc4e0fe2a22eceef8bdc4e9e7727
shawn-grant <shawngrant333@gmail.com>
2022-02-13 02:16:43 -0500

    windows icon works

--- src/devfolder.js
+++ src/devfolder.js
@@ -1,53 +1,134 @@
 #!/usr/bin/env node
 import inquirer from 'inquirer';
 import chalk from 'chalk';
+import { exec } from 'child_process';
 import * as fs from 'fs';
+import * as path from 'path'
+import * as url from 'url'
 
+// ask user for the folder target
 async function askTarget() {
     const answer = await inquirer.prompt({
         name: 'target',
         type: 'list',
-        message: 'Which folder(s) should be affected?',
+        message: 'Choose folder(s) to target',
         choices: [
-            'current folder',
-            'sub folders'
+            {
+                name: 'Current folder only',
+                value: 'current',
+            },
+            {
+                name: 'All subfolders (one level deep)',
+                value: 'sub',
+            }
         ]
     })
 
     return answer.target
 }
 
+// ask the user for the icon
 async function askIcon() {
     const answer = await inquirer.prompt({
         name: 'icon',
         type: 'list',
         message: 'Choose an icon',
-        choices: [
-            'current folder',
-            'sub folders'
-        ]
+        choices: await getAvailableIconsList()
     })
 
     return answer.icon
 }
 
-function changeFolderIcons(folderTarget) {
-    if (process.platform === "win32") {
-        // CHANGE ICON ON WINDOWS
+// change the icons for the target folders
+async function changeFolderIcons(folderTarget, iconName) {
+    console.log()
+
+    // current folder only
+    if (folderTarget == "current") {
+        changeIcon(process.cwd(), iconName)
+        return;
     }
-    else if (process.platform === "linux"){
-        // CHANGE ICON ON LINUX
-        
-    }
-    else if (process.platform === "darwin"){
-        // CHANGE ICON ON MACOS
-        
-    }
-    else {
-        console.log(chalk.red("Devfolders not supported on your OS"))
+
+    // subfolders (1 level deep)
+    if (folderTarget == "sub") { 
+        const folders = await getSubfolders()
+        folders.forEach(folder => changeIcon(path.join(process.cwd(), folder), iconName))
+        return;
     }
 }
 
+// list the icons available
+async function getAvailableIconsList() {
+    let list = []
+    const projectPath = url.fileURLToPath(import.meta.url.replace('src/devfolder.js', ''))
+    
+    try {
+        // assuming every OS will have the same icons available
+        const results = await fs.readdirSync(path.join(projectPath, 'icons', 'windows'))
 
+        results.forEach((file) => {
+            list.push(file.split('.')[0])
+        })
+    }
+    catch (error) {
+        console.log(error)
+    }
 
-export {askTarget, askIcon, changeFolderIcons}
+    return list
+}
+
+// returns a list of subfolders in the CWD
+async function getSubfolders() {
+    let list = []
+
+    try {
+        // assuming every OS will have the same icons available
+        await fs.readdirSync(process.cwd(), { withFileTypes: true })
+            .filter(dirent => dirent.isDirectory())
+            .map(dirent => dirent.name)
+            .forEach((folderName) => {
+                list.push(folderName)
+            })
+    }
+    catch (error) {
+        console.log(chalk.red(error))
+    }
+
+    return list
+}
+
+// actually change icon, called once, for each folder
+async function changeIcon(folderPath, iconName) {
+    console.log(chalk.yellow('Changing icon for ' + folderPath))
+
+    const projectPath = url.fileURLToPath(import.meta.url.replace('src/devfolder.js', ''))
+    const iconPath = path.join(projectPath, 'icons', 'windows', iconName)
+
+    if (process.platform == 'win32') {
+        try {
+            // create the icon config file
+            await fs.writeFileSync(path.join(folderPath, 'desktop.ini'),
+                '[.ShellClassInfo]\n' +
+                'IconResource=' + iconPath + '.ico\n' +
+                'IconIndex=0')
+            
+            // run commands to let the system recognize the file
+            exec('attrib +r ' + folderPath)
+            exec('attrib +s +h ' + folderPath + '/desktop.ini')
+        }
+        catch (error) {
+            console.log(chalk.red(error))
+        }
+    }
+    else if (process.platform == 'linux') {
+        
+    }
+    else if (process.platform == 'darwin') {
+        
+    }
+    else {
+        console.log(chalk.red("\nDevfolders not supported on your OS"))
+    }
+}
+
+export {askTarget, askIcon, changeFolderIcons, getAvailableIconsList}
--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@
 // attrib +s +h "path_to_folder/desktop.ini"
 
 // you may want to ignore it in .gitignore files
+import latestVersion from 'latest-version';
+import compareVersions from 'compare-versions';
 import commandLineArgs from 'command-line-args';
 import chalk from 'chalk';
-import {askTarget, askIcon, changeFolderIcons} from './devfolder.js'
+import {askTarget, askIcon, changeFolderIcons, getAvailableIconsList} from './devfolder.js'
+
+const CURRENT_VERSION = '1.0.3'
 
 const optionDefinitions = [
     { name: 'command', type: String, defaultOption: true },
@@ -23,25 +27,42 @@ const title =
     chalk.green("DER") + chalk.white(" Icons") + chalk.yellow("  \n")) +
     chalk.red("\t----#") + chalk.blue("----#") + chalk.green("----#") + chalk.yellow("----\n");
         
-
-// DO THE STUFF
+// SHOW THE TITLE
 console.clear()
 console.log(title)
-try {
-    const options = commandLineArgs(optionDefinitions)
-
-    let commandArg = options.command || 'none'
-    let versionArg = options.version || false
-    let helpArg = options.help || false
-    let targetArg = options.target || 'none'
-    let iconArg = options.icon || 'none'
     
+// PARSE AND HANDLE COMMAND LINE ARGUMENTS
+try {
+    const cliOptions = commandLineArgs(optionDefinitions)
+    const icons = await getAvailableIconsList()
+    let folderTarget = ''
+    let iconName = ''
+
     // if no arguments, ask question
-    if (Object.entries(options).length == 0) {
-        await askTarget()
-        await askIcon()
-        await changeFolderIcons()
-        process.exit(0)
+    if (Object.entries(cliOptions).length == 0) {
+        folderTarget = await askTarget()
+        iconName = await askIcon()
+        await changeFolderIcons(folderTarget, iconName)
+        await exitCLI()
+    }
+
+    let versionArg = cliOptions.version || false
+    let helpArg = cliOptions.help || false
+    let commandArg = cliOptions.command || 'none'
+    let targetArg = cliOptions.target || 'none'
+    let iconArg = cliOptions.icon || 'none'
+
+    // show the version. Other args are ignored
+    if (versionArg) {
+        console.log(chalk.yellow("Devfolders version " + CURRENT_VERSION))
+        await exitCLI()
+    }
+    
+    // show usage info. Other args are ignored
+    if (helpArg) {
+        console.log("Usage:")
+        console.log("")
+        await exitCLI()
     }
 
     // handle list and remove commands
@@ -49,6 +70,8 @@ try {
         switch (commandArg) {
             case 'list':
                 // show a list of icons available
+                console.log(chalk.bold("Available Icons:"))
+                icons.forEach(icon => console.log(icon))
                 break;
             
             case 'remove':
@@ -56,12 +79,51 @@ try {
                 break;
         }
 
-        process.exit(0)
+        await exitCLI()
     }
 
+    // when flags are passed
+    console.log('Target: ' + targetArg + ', Icon: ' + iconArg + '\n')
+
+    if (targetArg != 'current' && targetArg != 'sub') {
+        console.log(chalk.yellow('invalid target'))
+        folderTarget = await askTarget()      
+    }
+    else {
+        folderTarget = targetArg
+    }
+
+    if (icons.includes(iconArg) == false) {
+        console.log(chalk.yellow('invalid icon'))
+        iconName = await askIcon()
+    }
+    else {
+        iconName = iconArg
+    }
+
+    await changeFolderIcons(folderTarget, iconName)
+    await exitCLI()
 }
 catch (e) {
     // unknown args
     console.log(e)
     console.log(chalk.red("Invalid options"))
+    await exitCLI()
+}
+
+async function checkForUpdate() {
+    try {
+        const latestVer = await latestVersion('devfolders')
+    
+        if(compareVersions(latestVer, CURRENT_VERSION) == 1)
+            console.log('\n*New version available (' + latestVer + '). ' +
+            '\nRun "npm install -g devfolders" to update');
+    }
+    catch (err) { 
+    }
+}
+
+async function exitCLI() {
+    await checkForUpdate()
+    process.exit(0)
 }
